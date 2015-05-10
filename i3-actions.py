c7fbf518ca68e85f5a59585215bf0ad08c19d99f
T.B <infy@cybershade.org>
2015-05-10 11:38:31 +0200

    Added main output support so you don't bounce around, added some unfinished function prototypes. Fixed how windows are jumped to, now you can jump to any window and perhaps any dialog (untested). Some minor bugfixes.

--- i3-actions.py
+++ i3-actions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 __version__ = '1.4'
+__verbose__ = True
 
 import sys
 import subprocess
@@ -9,13 +10,14 @@ from subprocess import Popen, PIPE
 try:
     import i3ipc
 except ImportError:
-    print('i3-modp cannot work without i3ipc. Exiting...')
+    print('i3-actions cannot work without i3ipc. Exiting...')
     exit()
 
 class i3actions(object):
 
     def __init__(self):
         self.dmenu_args = ['/usr/bin/dmenu'] + ['-b', '-i'] # Refer to docs about this one. Make it more appealing here.
+        self.main_output = 'CRT2'
 
         # Handle the input argument
         if len(sys.argv) < 2:
@@ -39,38 +41,49 @@ class i3actions(object):
 
         sys.exit(0) # Maybe you can chain this with some unix tools?
 
-    def _dmenu(self, data, lines):
-        data = bytes(str.join('\n', data), 'UTF-8') # Join all the newline and UTF-8 encode it.
+    def _dmenu(self, data, lines, prompt = None):
+        ddata = bytes(str.join('\n', list(data.values())), 'UTF-8') # Join all the newline and UTF-8 encode it.
+
         try:
+            if prompt: self.dmenu_args = self.dmenu_args + ['-p', prompt]
             p = Popen(self.dmenu_args + ['-l', str(lines)], stdin=PIPE, stdout=PIPE, stderr=PIPE)
         except ValueError as e:
             print('Popen construct failed: %s' % e.message)
             sys.exit(1)
         except OSError as e:
             print('OSError: %s' % e.message)
+            sys.exit(1)
 
-        stdout, stderr = p.communicate(data)
+        stdout, stderr = p.communicate(ddata)
         if(stderr): print('stderr: %s' % stderr) # Could be useful for debugging?
 
-        return stdout
+        # Decode and strip newline from dmenu's return
+        stdout = stdout.decode('UTF-8').strip()
+
+        # We need to get the key by the value the user chooses. Remember the key is the ID, the value is the actual name.
+        for id, name in data.items():
+            if name == stdout: # They match
+                return id # Return the ID to the parent (caller) function
 
     def _get_window_names(self):
         outputs = self.connection.get_tree().leaves()
 
-        windows = []
+        windows = {}
         for w in outputs:
-            windows.append(w.name)
+            if w.name in windows: continue # This makes sure there are no duplicates. Refer to docs about this one.
+            windows[w.id] = w.name
 
         return windows
 
     def jump_to(self):
         windows = self._get_window_names()
 
-        # Send the command, receive it and decode it.
-        cmd = self._dmenu(windows, len(windows)).decode('UTF-8').strip()
+        # Send the command, receive the output. This will return the ID of the window, of course.
+        cmd = self._dmenu(windows, len(windows))
+        if __verbose__: print(cmd)
 
         # Just execute...
-        self.connection.command('[title="%s"] focus' % cmd)
+        self.connection.command('[con_id="%d"] focus' % cmd)
 
     def move_here(self):
         # Get all the windows
@@ -97,13 +110,28 @@ class i3actions(object):
         workspaces = []
         i = 1
         for ws in self.connection.get_workspaces():
-            if i == ws.num:
+            if i == ws.num and ws.output == self.main_output:
                 i = i+1
                 continue
             else:
                 break
 
         self.connection.command('workspace number %d' % i)
+
+    def kill(self):
+        pass
+
+    def rename(self):
+        # Get all the workspaces
+        workspaces = self.connection.get_workspaces()
+
+        # Find the current workspace ID, not the name
+        for ws in workspaces:
+            if(ws.focused): ws_current = ws.num
+
+        # Prompt the user for a new name
+        #self._dmenu(0, )
+        pass
 
 if __name__ == '__main__':
     i3actions()
